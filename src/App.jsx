7a182731ffa554f5fee78b20d8fe5c729708b9ec
sanjay <sanjaykumar73189@gmail.com>
2024-12-15 14:47:05 +0530

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import Hero from './Components/Hero'
-
+import { useState } from "react";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
 
 function App() {
-
   return (
     <>
-      <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <main className="relative min-h-screen w-screen overflow-x-hidden">
         <Hero />
+        <About />
       </main>
-      <section className='z-0 min-h-screen bg-violet-300'></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
